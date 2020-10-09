35253109265d97c58bc6880c41b4295ef0f86c30
turtl01 <bnfxt@mail.ru>
2020-10-09 03:11:44 +0300

    fix

--- index.php
+++ index.php
@@ -7,14 +7,7 @@
       $data = fgetcsv($file, 5000, ';');
       $j = count($data);
       $mysql -> query("INSERT INTO `testtest` (`id`, `st1`, `st2`, `st3`, `st4`) VALUES (NULL, '{$data[0]}', '{$data[1]}', '{$data[2]}', '{$data[3]}');");
-        // code...
-      print_r($data);
-
-
   }
-
     fclose($file);
     $mysql->close();
-    // $array2 = array('1'=>array('2'=>2));
-    // var_dump($array2);
   }
